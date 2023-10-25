1b806060f03053a7b88df576485b8315ebd42e56
Ishika <ishikadubey792@gmail.com>
2023-10-25 10:50:13 +0530

    contactManager

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes,Route,Navigate} from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import ContactList from './components/contacts/contactList/ContactList';
+import AddContact from './components/contacts/addContact/AddContact';
+import ViewContact from './components/contacts/viewContact/ViewContact';
+import EditContact from './components/contacts/editContact/EditContact';
 
-function App() {
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+        <Navbar/>
+        <Routes>
+            <Route path={'/'} element={<Navigate to={'/contacts/list'}/>}/>
+            <Route path={'/contacts/List'} element={<ContactList/>}/>
+            <Route path={'/contacts/add'} element={<AddContact/>}/>
+            <Route path={'/contacts/view/:contactId'} element={<ViewContact/>}/>
+            <Route path={'/contacts/edit/:contactId'} element={<EditContact/>}/>
+        </Routes>
+    </>
   );
 }
 
